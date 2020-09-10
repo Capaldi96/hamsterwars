56fd93b07dfe5d868dc9304898d311a39ba8d968
alexandrajampolskaia <a.jampolskaia@gmail.com>
2020-09-10 23:24:09 +0200

    added circle pic

---
+++ src/components/jsx/Statistic.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Statistic() {
+	return (
+		<div>
+
+		<img className="statCircle" src={require('./assets/bigcircle_gamesplayed.png')} alt="circle"/>
+			
+		</div>
+	)
+}
