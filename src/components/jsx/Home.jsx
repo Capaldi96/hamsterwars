7d15ab22676e5ee75fbecdc6ba469b893a8435b4
Hanna Möllergren <hannamollergren@gmail.com>
2020-09-10 16:04:35 +0200

    home.jsx elements

--- src/components/jsx/Home.jsx
+++ src/components/jsx/Home.jsx
@@ -5,8 +5,13 @@ function Home() {
 	return (
 	  <div>
 		<main className="home-main">
+			
 			<div className="home-container">
-
+				<img  className="hamster-icon" src={require('../../assets/hamster.png')} alt="Hamster Icon"/>
+				<h1>VOTE FOR<br></br> THE CUTEST<br></br> HAMSTER.</h1>
+				<p>Welcome to HamsterWars!<br></br> Vote on the hamster you like the most. <br></br>Get info about how many battles won, lost or other stuff.</p>
+				<button>Go to battle</button>
+				
 			</div>
 		</main>
 	  </div>
