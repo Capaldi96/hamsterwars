a30703f341c83b93377049ea4c08d1e13ba7c27a
Hanna Möllergren <hannamollergren@gmail.com>
2020-09-10 10:18:02 +0200

    SASS files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import { Counter } from './features/counter/Counter';
-import './App.css';
+import './App.scss';
+import Home from '../src/components/jsx/Home';
 
 
 function App() {
@@ -8,15 +9,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <nav>
-          <a>Home</a>
-          <a>Battle</a>
-          <a>Add hamster</a>
-          <a>Statistic</a>
+          <a href="Home.jsx">Home</a>
+          <a href="BattleResult.jsx">Battle</a>
+          <a href="Form.jsx">Add hamster</a>
+          <a href="Statistic.jsx">Statistic</a>
         </nav>
-        <img className="waves" src={require('./assets/top_waves.png')} alt="asd"/>
+        <img className="waves" src={require('./assets/top_waves.png')} alt="wave"/>
       </header>
+	  <Home/>
       <footer className="App-footer">
-        <img className="waves" src={require('./assets/bottom_waves.png')} alt="asd"/>
+        <img className="waves" src={require('./assets/bottom_waves.png')} alt="wave"/>
       </footer>
     </div>
   );
