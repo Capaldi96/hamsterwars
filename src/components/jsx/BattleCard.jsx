4ce71236d3a0c7cf6820cdca02f5e79e3870a4b2
Emil Hammarstrand <emilhammarstrand@hotmail.com>
2020-09-11 11:43:02 +0200

    Me and jonas working on battleresult

---
+++ src/components/jsx/BattleCard.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import '../scss/BattleCard.scss';
+
+const BattleCard = () => {
+	return (
+		<div className="flip-card">
+			<img className="flip-card-info" src={require('../../assets/info.svg')} />
+			<div className="flip-card-inner">
+				<div className="flip-card-front">
+					<img
+						className="competitior-img"
+						src="https://i.pinimg.com/originals/04/d2/34/04d23484b0a7ab2c1840b4c6a7cf78c7.jpg"
+						alt="Avatar"
+					/>
+				</div>
+				<div className="flip-card-back">
+					<h1>Hamter</h1>
+					<p>Carrot eater</p>
+					<p>We love that hampter</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+
+export default BattleCard;
--- src/components/jsx/BattleResult.jsx
+++ src/components/jsx/BattleResult.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import '../scss/BattleResult.scss';
-
+import BattleCard from './BattleCard'
 const BattleResult = () => {
 
 
@@ -9,35 +9,11 @@ const BattleResult = () => {
 			<div className="container">
 				<h1>Choose your cutest</h1>
 				<div className="match-container">
-					<div className="flip-card">
-						<div className="flip-card-inner">
-							<img className="flip-card-info" src={require('../../assets/info.svg')}></img>
-							<div className="flip-card-front">
-								<img className="competitior-img" src="https://i.pinimg.com/originals/04/d2/34/04d23484b0a7ab2c1840b4c6a7cf78c7.jpg" alt="Avatar" ></img>
-							</div>
-							<div className="flip-card-back">
-								<h1>Hamter</h1>
-								<p>Carrot eater</p>
-								<p>We love that hampter</p>
-							</div>
-						</div>
-					</div>
+                    <BattleCard></BattleCard>
 					<img className="VS" src={require('../../assets/vs.png')}></img>
-					<div className="flip-card">
-						<div className="flip-card-inner">
-							<img className="flip-card-info" src={require('../../assets/info.svg')}></img>
-							<div className="flip-card-front">
-								<img className="competitior-img" src="https://i.pinimg.com/originals/04/d2/34/04d23484b0a7ab2c1840b4c6a7cf78c7.jpg" alt="Avatar" ></img>
-							</div>
-							<div className="flip-card-back">
-								<h1>Hamter</h1>
-								<p>Carrot eater</p>
-								<p>We love that hampter</p>
-							</div>
-						</div>
-					</div>
+					<BattleCard></BattleCard>
 				</div>
-				<div>1		</div>
+				<div className="resultPotato"></div>
 			</div>
 		</div>
 	);
