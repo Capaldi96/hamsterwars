97de99751fba6715c7ccfeabdf343999a81dc3ab
Karin Karlsson <karin.drake@gmail.com>
2020-09-11 13:15:28 +0200

    formpotato är på plats, inputfält samt labels, påbörjat för floating labels

--- src/components/jsx/Form.jsx
+++ src/components/jsx/Form.jsx
@@ -6,23 +6,69 @@ const Form=()=>{
 
 	// const [name, setName]=useState('')
 	const [name, setName]=useState('')
+	const [age, setAge]=useState('')
+	const [favoritefood, setFavoritefood]=useState('')
+	const [loves, setLoves]=useState('')
+	const [image, setImage]=useState('')
+
 
 
 	return(
 
-		<div className='Form'>
+		<div className='form-component'>
 			<div className='form-wrapper'>
 
+				<p>Add your own hamster</p>
+				<form>
 
-				{/* <form>
+					<div>
+						<input type='text'
+						id="name"
+						value={name}
+						onChange={event=>setName(event.target.value)}/>
+						<label for="name">Hamster name</label>
+					</div>
+					
+					<div>
+						<input type='text' 
+						id='age'
+						value={age}
+						onChange={event=>setAge(event.target.value)}/>
+						<label for="age">Hamster age</label>
+					</div>
+			
+					<div>
+						<input type='text'
+						id='favoritefood' 
+						value={favoritefood}
+						onChange={event=>setFavoritefood(event.target.value)}/>
+						<label for="favoritefood">Favoritefood</label>
+
+					</div>
+				
+					<div>
+						<input type='text'
+						id='loves' 
+						value={loves}
+						onChange={event=>setLoves(event.target.value)}/>
+						<label for="loves">Loves</label>
+
+					</div>
+				
+					<div>
+						<input type='text'
+						id='image' 
+						value={image}
+						onChange={event=>setImage(event.target.value)}/>
+						<label for="image">Image</label>
 
 
-					<input type='text' 
-					value={name}
-					onChange={event=>setName(event.target.value)}/>
-				</form> */}
-				{/* <img className="form-potatoe" src={require('../../assets/formpotatoe.svg')} alt="formbackground"/> */}
+					</div>
+				
 
+				</form>
+				<button>Add hamster</button>
+			
 
 			</div>
 					
