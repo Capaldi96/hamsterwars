6f342c59c5cd60ea973b2bfe3fdba5876828c52b
Hanna Möllergren <hannamollergren@gmail.com>
2020-09-13 17:12:27 +0200

    hamburger menu done, no function yet

--- src/components/jsx/Header.jsx
+++ src/components/jsx/Header.jsx
@@ -1,20 +1,44 @@
 import React from 'react';
 import '../scss/Header.scss';
 
-function Header() {
+const header = () => {
+
+	function toggle(){
+		console.log('toggle click')
+		const navbar = document.querySelector('.navbar');
+		navbar.classList.toggle('change');
+	}
+
 	return (
 	<div>
 		<header className="App-header">
-        	<nav>
-				<a href="Home.jsx">Home</a>
-				<a href="BattleResult.jsx">Battle</a>
-				<a href="Form.jsx">Add hamster</a>
-				<a href="Statistic.jsx">Statistic</a>
-        	</nav>
-        	<img className="top-wave" src={require('../../assets/top_waves.png')} alt="wave"/>
-      	</header>
+			<nav className="navbar"> 
+
+				<img className="mobile-logo" src={require('../../assets/hamster.png')} alt="logo"/>
+				<div className="hamburger-menu" onClick={toggle}>
+					<div className="line line1"></div>
+					<div className="line line2"></div>
+					<div className="line line3"></div>
+				</div>
+				<ul className="nav-list">
+					<li className="nav-item">
+					<a href="#" className="nav-link">Home</a>
+					</li>
+					<li className="nav-item">
+					<a href="#" className="nav-link">Battle</a>
+					</li>
+					<li className="nav-item">
+					<a href="#" className="nav-link">Add hamster</a>
+					</li>
+					<li className="nav-item">
+					<a href="#" className="nav-link">Statistic</a>
+					</li>
+				</ul>
+			</nav>
+			<img className="top-wave" src={require('../../assets/top_waves.png')} alt="wave"/>
+		</header> 
 	</div>
 	);
   }
   
-  export default Header;
+  export default header;
--- src/components/jsx/Home.jsx
+++ src/components/jsx/Home.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import '../scss/Home.scss';
 
-function Home() {
+const home = () => {
 	return (
 	  <div>
 		<main className="home-main">
-			
 			<div className="home-container">
 				<img  className="hamster-icon" src={require('../../assets/hamster.png')} alt="Hamster Icon"/>
 				<h1>VOTE FOR<br></br> THE CUTEST<br></br> HAMSTER.</h1>
-				<p>Welcome to HamsterWars!<br></br> Vote on the hamster you like the most. <br></br>Get info about how many battles won, lost or other stuff.</p>
+				<p>Welcome to HamsterWars!<br></br> Vote on the hamster you like the most. <br></br>Get info about how many battles won, lost or other fun stuff.</p>
 				<button>Go to battle</button>
-				
 			</div>
 		</main>
 	  </div>
 	);
   }
   
-  export default Home;
+  export default home;
