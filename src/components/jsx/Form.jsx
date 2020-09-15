b72bcb98629d9ab5b7d583849a6dabb8476918bb
Karin Karlsson <karin.drake@gmail.com>
2020-09-15 12:12:41 +0200

    parprog med hanna, lagt in validering i form

--- src/components/jsx/Form.jsx
+++ src/components/jsx/Form.jsx
@@ -1,4 +1,5 @@
 import React ,{useState} from 'react'
+import { useForm } from "react-hook-form";
 import '../scss/Form.scss'
 
 
@@ -11,7 +12,8 @@ const Form=()=>{
 	const [loves, setLoves]=useState('')
 	const [image, setImage]=useState('')
 
-
+	const { register, handleSubmit, errors } = useForm();
+	const onSubmit = data => console.log(data);
 
 	let newHamster = {
 		name:name,
@@ -59,7 +61,7 @@ const Form=()=>{
 			<div className='form-wrapper'>
 
 				<p>Add your own hamster</p>
-				<form className='form'>
+				<form className='form' onSubmit={handleSubmit(onSubmit)}>
 
 					<div className='form-group'>
 						<input type='text'
@@ -68,32 +70,44 @@ const Form=()=>{
 						className='form-control'
 						value={name}
 						placeholder='name placeholder'
+						ref={register({ required: true, maxLength:15, minLength:1 })}
 						onChange={event=>setName(event.target.value)}/>
 						<label htmlFor="name" className='form-label'>Name</label>
 					</div>
-					
+					{errors.name && errors.name.type==='required' && <span>This field is required</span>}
+					{errors.name && errors.name.type==='maxLength' && <span>Max 15 characters</span>}
+					{errors.name && errors.name.type==='minLength' && <span>Min 1 character</span>}
+				
 					<div className='form-group'>
-						<input type='text' 
+						<input type='number' 
 						id='age'
 						name='age'
 						className='form-control'
 						value={age}
 						placeholder='age placeholder'
+						ref={register({ required: true, maxLength:10, minLength:1 })}
 						onChange={event=>setAge(event.target.value)}/>
-						<label htmlFor="age" className='form-label'>Age</label>
+						<label htmlFor="age" className='form-label'>Age (3 mån=0.3. 1 år=1)</label>
 					</div>
+					{errors.age && errors.age.type==='required' && <span>This field is required</span>}
+					{errors.age && errors.age.type==='maxLength' && <span>Max 10 characters</span>}
+					{errors.age && errors.age.type==='minLength' && <span>Min 1 character</span>}
 			
 					<div className='form-group'>
 						<input type='text'
-						id='favoritefood'
-						name='favoritefood'
+						id='favFood'
+						name='favFood'
 						className='form-control' 
 						value={favFood}
 						placeholder='food placeholder'
+						ref={register({ required: true, maxLength:10, minLength:2 })}
 						onChange={event=>setFavFood(event.target.value)}/>
-						<label htmlFor="favoritefood" className='form-label'>Favorite food</label>
+						<label htmlFor="favFood" className='form-label'>Favorite food</label>
 
 					</div>
+					{errors.favFood && errors.favFood.type==='required' && <span>This field is required</span>}
+					{errors.favFood && errors.favFood.type==='maxLength' && <span>Max 10 characters</span>}
+					{errors.favFood && errors.favFood.type==='minLength' && <span>Min 2 character</span>}
 				
 					<div className='form-group'>
 						<input type='text'
@@ -102,10 +116,14 @@ const Form=()=>{
 						name='loves' 
 						value={loves}
 						placeholder='love placeholder'
+						ref={register({ required: true, maxLength:15, minLength:2 })}
 						onChange={event=>setLoves(event.target.value)}/>
 						<label htmlFor="loves" className='form-label'>Loves</label>
 
 					</div>
+					{errors.loves && errors.loves.type==='required' && <span>This field is required</span>}
+					{errors.loves && errors.loves.type==='maxLength' && <span>Max 15 characters</span>}
+					{errors.loves && errors.loves.type==='minLength' && <span>Min 2 character</span>}
 				
 					<div className='form-group'>
 						<input type='text'
@@ -114,12 +132,14 @@ const Form=()=>{
 						name='image' 
 						value={image}
 						placeholder='image placeholder'
+						ref={register({ required: true })}
 						onChange={event=>setImage(event.target.value)}/>
 						<label htmlFor="image" className='form-label'>Image</label>
 
 					</div>
+					{errors.image && errors.image.type==='required' && <span>This field is required</span>}
 				
-
+					<input type="submit" />
 				</form>
 				<button onClick={addHamster}>Add hamster</button>
 			
