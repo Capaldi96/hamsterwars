9c776a6340fce9a5c4148c9dfdd40d6fcf7006ae
alexandrajampolskaia <a.jampolskaia@gmail.com>
2020-09-16 11:50:08 +0200

    added comments to statistics

--- src/components/jsx/Statistic.jsx
+++ src/components/jsx/Statistic.jsx
@@ -24,11 +24,12 @@ const [showLatestBattles, setShowLatestBattles] = useState(false);
 <div className="circlesInRow">
 
 {/* Games played */}
+{/* Games played - fade potatoe */}
 <FadeEffectGamesPlayed showGamesPlayed={showGamesPlayed}>
 <div className="ellipsbackground" onClick={() => setShowGamesPlayed(show => !showGamesPlayed)}>
 <img className="ellipse" src={require('../../assets/ellipseGamesPlayed.svg')} alt=""/></div>
 </FadeEffectGamesPlayed> 
-
+{/* Games played - green circles */}
 <div className="statCircle1 circleSpecifics" onClick={() => setShowGamesPlayed(showGamesPlayed => !showGamesPlayed)}>
 <img className="iconInsideCircle" src={require('../../assets/iconGamesPlayed.svg')} alt=""/> 
 <img className="miniIconInsideCircle" src={require('../../assets/miniIconGamesPlayed.svg')} alt=""/></div>
@@ -36,11 +37,12 @@ const [showLatestBattles, setShowLatestBattles] = useState(false);
 
 
 {/* Top 5 winners */}
+{/* Top 5 winners - fade potatoe */}
 <FadeEffectTop5Winners showTop5Winners={showTop5Winners}>
 <div className="ellipsbackground" onClick={() => setShowTop5Winners(showTop5Winners => !showTop5Winners)}>
 <img className="ellipse" src={require('../../assets/ellipseTop5Winners.svg')} alt=""/></div>
 </FadeEffectTop5Winners> 
-
+{/* Top 5 winners - green circles */}
 <div className="statCircle2 circleSpecifics" onClick={() => setShowTop5Winners(showTop5Winners => !showTop5Winners)}>
 <img className="iconInsideCircle" src={require('../../assets/iconTop5Winners.svg')} alt=""/> 
 <img className="miniIconInsideCircle" src={require('../../assets/miniIconTop5Winners.svg')} alt=""/></div>
@@ -48,41 +50,44 @@ const [showLatestBattles, setShowLatestBattles] = useState(false);
 
 
 {/* Top 5 loosers */}
+{/* Top 5 loosers - fade potatoe */}
 <FadeEffectTop5Loosers showTop5Loosers={showTop5Loosers}>
 <div className="ellipsbackground" onClick={() => setShowTop5Loosers(showTop5Loosers => !showTop5Loosers)}>
-<GetTopLoosers />
-</div>
+<GetTopLoosers /></div>
 </FadeEffectTop5Loosers> 
-
+{/* Top 5 loosers - green circles */}
 <div className="statCircle3 circleSpecifics" onClick={() => setShowTop5Loosers(showTop5Loosers => !showTop5Loosers)}>
 <img className="iconInsideCircle" src={require('../../assets/iconTop5Loosers.svg')} alt=""/> 
 <img className="miniIconInsideCircle" src={require('../../assets/miniIconTop5Loosers.svg')} alt=""/></div>
 <p className="miniIconText">Top 5 Loosers</p>
 
+
 {/* Least participated */}
+{/* Least participated - fade potatoe */}
 <FadeEffectLeastParticipated showLeastParticipated={showLeastParticipated}>
 <div className="ellipsbackground" onClick={() => setShowLeastParticipated(showLeastParticipated => !showLeastParticipated)}>
 <img className="ellipse" src={require('../../assets/ellipseLeastParticipated.svg')} alt=""/></div>
 </FadeEffectLeastParticipated> 
-
+{/* Least participated - green circles */}
 <div className="statCircle4 circleSpecifics" onClick={() => setShowLeastParticipated(showLeastParticipated => !showLeastParticipated)}>
 <img className="iconInsideCircle" src={require('../../assets/iconLeastParticipated.svg')} alt=""/> 
 <img className="miniIconInsideCircle" src={require('../../assets/miniIconLeastParticipated.svg')} alt=""/></div>
 <p className="miniIconText">Least participated</p>
 
+
 {/* Latest battles */}
+{/* Latest battles - fade potatoe */}
 <FadeEffectLatestBattles showLatestBattles={showLatestBattles}>
 <div className="ellipsbackground" onClick={() => setShowLatestBattles(showLatestBattles => !showLatestBattles)}>
 <img className="ellipse" src={require('../../assets/ellipseLatestBattles.svg')} alt=""/></div>
 </FadeEffectLatestBattles> 
-
+{/* Latest battles - green circles */}
 <div className="statCircle5 circleSpecifics" onClick={() => setShowLatestBattles(showLatestBattles => !showLatestBattles)}>
 <img className="iconInsideCircle" src={require('../../assets/iconLatestBattles.svg')} alt=""/> 
 <img className="miniIconInsideCircle" src={require('../../assets/miniIconLatestBattles.svg')} alt=""/></div>
 <p className="miniIconText">Latests battles</p>
 
 </div>
-
 </div>
 	)
 }
