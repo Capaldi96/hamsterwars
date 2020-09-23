489919a8e4182b55e0a0746e356bb76675da4a52
Hanna Möllergren <hannamollergren@gmail.com>
2020-09-23 12:37:53 +0200

    html elemnt flipcard gallery

--- src/components/jsx/Gallery.jsx
+++ src/components/jsx/Gallery.jsx
@@ -5,7 +5,8 @@ import '../scss/Gallery.scss';
 const Gallery = () => {
 
 	const [hamsterList, setHamsterList] = useState([]);
-	const [message, setMessage] = useState('Delete');
+	const [statusDelete, setStatusDelete] = useState('Delete');
+
 
 	useEffect(() => {
 		getHamsters();
@@ -28,25 +29,67 @@ const Gallery = () => {
 		console.log(response)
 		getHamsters();
 	}
+
+	let status = null;
+	if (!hamsterList.length) {
+		status = <div className="loading"><h2>Loading...</h2></div>;
+	} 
+	else {
+		status = 
+			<main>
+				<div className="container-list">
+					<div className="flip-card-gallery">
+						<div className="flip-card-inner-gallery">
+							{/* FRONT */}
+							{hamsterList.map(hamster => (
+							<div  key={hamster._id}className="flip-card-front-gallery list">
+								<img src={hamster.imgName} alt="Hamster" className="hamster-image"/>
+							</div>))}
+							{/* BACK */}
+							{hamsterList.map(hamster => (
+							<div key={hamster._id}className="flip-card-back-gallery list">
+								<button onClick={() => deleteHamster(hamster._id)}>{statusDelete}</button>
+								<p><span>{hamster.name}</span></p>
+								<p>Years: {hamster.age}</p>
+								<p>Games: {hamster.games}</p> 
+								<p>Favfood: {hamster.favFood}</p> 
+								<p>Loves: {hamster.loves}</p>
+								<p>Wins: {hamster.wins}</p> 
+								<p>Losses: {hamster.defeats}</p>
+							</div>))}
+						</div>
+					</div>
+				</div>
+			</main>
+		
+	}
 	
 
 	return (
 		<div className="Gallery">
-			<main>
-				<div className="container-list">
-					{hamsterList.map(hamster => (
-					<div key={hamster._id} className="list">
-						<button onClick={() => deleteHamster(hamster._id)}>{message}</button>
-						<p ><span>{hamster.name}</span></p>
-						<p>Years: {hamster.age}</p>
-						<p>Games: {hamster.games}</p> 
-						<p>Favfood: {hamster.favFood}</p> 
-						<p>Loves: {hamster.loves}</p>
-						<p>Wins: {hamster.wins}</p> 
-						<p>Loses: {hamster.defeats}</p>
-					</div>))}
-				</div>
-			</main>
+		{/* 	<main>
+					<div className="container-list">
+						{hamsterList.map(hamster => (
+						<div key={hamster._id} className="list font">
+							<img src={hamster.imgName} alt="Hamster" className="hamster-image"/>
+						</div>))}
+					</div>
+					<div className="container-list">
+						{hamsterList.map(hamster => (
+						<div key={hamster._id} className="list back">
+							<button onClick={() => deleteHamster(hamster._id)}>{message}</button>
+							<p ><span>{hamster.name}</span></p>
+							<p>Years: {hamster.age}</p>
+							<p>Games: {hamster.games}</p> 
+							<p>Favfood: {hamster.favFood}</p> 
+							<p>Loves: {hamster.loves}</p>
+							<p>Wins: {hamster.wins}</p> 
+							<p>Losses: {hamster.defeats}</p>
+						</div>))}
+					</div>
+			</main> */}
+			<div>{status}</div>
+
 		</div>
 	)
 }
